79285323b6c2cfdc69defbdf38149e267eee517a
Gaurav@123 <mr.gauravkumar8757@gmail.com>
2024-06-24 13:08:21 +0530

    image path set src/assets to public

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <header className="sticky top-0 p-4 bg-gradient-to-r from-cyan-950 to-blue-900 text-white flex xl:justify-between justify-evenly items-center"> 
             <Link to={`/`} className="flex my-auto  justify-center">
-            <img src= "/src/assets/gallery.png" className="h-10 w-10 mr-3" alt="logo" />
+            <img src= "/gallery.png" className="h-10 w-10 mr-3" alt="logo" />
             <h1 className="text-3xl font-mono font-bold content-center uppercase">Albums List</h1>
             </Link>   
             <NetworkStatus/>
